be4b8f8d19c58e4c1e339b461f210e6676351312
An-Ran Lee <alee208@ucsc.edu>
2023-10-07 23:07:53 -0700

    Step 1 complete

--- src/infiniteEducation.ts
+++ src/infiniteEducation.ts
@@ -9,3 +9,18 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// adds game container
+const gameDiv: HTMLDivElement = document.createElement("div");
+gameDiv.id = "gameContainer";
+app.append(gameDiv);
+
+const scoreDisplay: HTMLDivElement = document.createElement("div");
+scoreDisplay.id = "scoreDisplay";
+gameDiv.append(scoreDisplay);
+
+// adds first button
+const bookButton: HTMLButtonElement = document.createElement("button");
+bookButton.innerHTML = "📖";
+bookButton.id = "bookButton";
+gameDiv.append(bookButton);
